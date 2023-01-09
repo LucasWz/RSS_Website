1416c3ac657a6c1f804e667bf6cdd6412c6f6306
Lucas WINTZ <lucas.wintz@laposte.net>
2023-01-09 11:45:16 -0400

    add search bar backend

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 from flask import Flask, render_template
 import feedparser
-import time
+from fuzzywuzzy import fuzz
 
 app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-  feed_url = "https://www.mediapart.fr/articles/feed"
-  feed = parse_feed(feed_url)
-  sorted_entries = sort_feed_entries_by_date(feed)
-  return render_template("home.html", entries=sorted_entries)
 
 def parse_feed(feed_url):
   feed = feedparser.parse(feed_url)
@@ -19,6 +11,23 @@ def parse_feed(feed_url):
 def sort_feed_entries_by_date(feed):
   sorted_entries = sorted(feed.entries, key=lambda entry: entry.published_parsed, reverse=True)
   return sorted_entries
+
+def fuzzy_search(query, entries):
+  query = query.lower()
+  matching_entries = []
+  for entry in entries:
+    title = entry.title.lower()
+    if fuzz.token_set_ratio(query, title) >= 90:
+      matching_entries.append(entry)
+  return matching_entries
+
+@app.route("/")
+def home():
+  feed_url = "https://www.mediapart.fr/articles/feed"
+  feed = parse_feed(feed_url)
+  sorted_entries = sort_feed_entries_by_date(feed)
+  return render_template("home.html", entries=sorted_entries)
+
 
 if __name__ == "__main__":
   app.run()
